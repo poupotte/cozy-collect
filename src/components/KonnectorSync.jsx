5be56644919e46160166c0715b61ca6f424ef717
Patrick Browne <pt.browne@gmail.com>
2018-04-20 12:58:14 +0200

    ✨ feat: log the schedule execution of the konnector

--- src/components/KonnectorSync.jsx
+++ src/components/KonnectorSync.jsx
@@ -11,6 +11,38 @@ function getDateLabel({ date, t, f }) {
   return f(DateFns.parse(date), t('account.message.synced.date_format'))
 }
 
+const months = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec'
+]
+const dows = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+
+const humanCron = cronSpec => {
+  try {
+    const [, minute, hour, day, month, dayOfWeek] = cronSpec.split(' ')
+    const timePart =
+      hour !== '*' && minute !== '*'
+        ? `at ${hour}:${minute}`
+        : hour !== '*' ? `${hour}h` : `Any hour on ${minute}minute`
+    const monthPart = month !== '*' ? `during ${months[month]}` : null
+    const dowPart = dayOfWeek !== '*' ? `on ${dows[dayOfWeek]}` : null
+    const dayPart = day !== '*' ? `on ${day}th of the month` : null
+    return [timePart, monthPart, dowPart, dayPart].filter(Boolean).join(' ')
+  } catch (e) {
+    return JSON.stringify(e)
+  }
+}
+
 export const KonnectorSync = ({
   t,
   f,
@@ -18,13 +50,15 @@ export const KonnectorSync = ({
   lastSuccessDate,
   maintenance,
   submitting,
-  onForceConnection
+  onForceConnection,
+  trigger
 }) => {
   const lastSyncMessage =
     (submitting && t('account.message.synced.syncing')) ||
     (!lastSuccessDate && t('account.message.synced.unknown')) ||
     (lastSuccessDate && getDateLabel({ date: lastSuccessDate, t, f })) ||
     null
+  console.log('Scheduled execution :', humanCron(trigger.arguments))
   return (
     <div>
       {
